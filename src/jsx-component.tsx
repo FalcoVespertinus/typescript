8b1da644ccd529c7470e487a0b41f377a6d44e51
Dave Decker <david.decker@stud.hslu.ch>
2024-05-29 20:32:00 +0200

    rename to fit chapters

--- src/jsx-component.tsx
+++ src/jsx-component.tsx
@@ -1,4 +1,4 @@
-import { destructuringFunction } from "./function-destructuring";
+import { destructuringFunction } from "./parameter-destructuring";
 
 export default function JsxComponent() {
   const nameAndAge = destructuringFunction({ name: "john", age: 12 });
