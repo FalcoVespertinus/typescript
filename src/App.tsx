55e0ec9acb15e64468665d6dc58ea0f3830d112c
Dave Decker <david.decker@stud.hslu.ch>
2024-05-29 20:45:53 +0200

    add optional chaining

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import "./App.css";
 import JsxComponent from "./jsx-component";
+import { petNames } from "./optional-chaining";
 
 function App() {
+  console.log(petNames);
+
   return (
     <div className="App">
       <JsxComponent />
---
+++ src/optional-chaining.ts
@@ -0,0 +1,15 @@
+type Person = {
+  name: string;
+  age: number;
+  pet?: {
+    name: string;
+  };
+};
+
+const person1: Person = { name: "john", age: 12 };
+const person2: Person = { name: "susan", age: 10, pet: { name: "fluffy" } };
+
+export const petNames = [person1, person2].map(({ pet }) => pet?.name);
+
+console.log(petNames);
+// output: [undefined, 'fluffy']
